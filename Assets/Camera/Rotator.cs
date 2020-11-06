2ea557578168f72fbe5194d44bae1b9c72832f86
m4rc3l05 <joaobat@live.com.pt>
2020-11-06 23:11:41 +0000

    Add camera rotator
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ Assets/Camera/Rotator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    Transform Center;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var canRotate = Input.GetMouseButton(0) && Input.GetKey(KeyCode.Space);
+
+        if (!canRotate)
+            return;
+
+        var mouseX = Input.GetAxisRaw("Mouse X");
+
+        this.transform.RotateAround(this.Center.position, Vector3.up, 10 * mouseX);
+    }
+}
