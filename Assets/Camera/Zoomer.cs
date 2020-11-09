efc1b29a024a404b33f205aa1b737dbcf6f1647f
m4rc3l05 <joaobat@live.com.pt>
2020-11-09 22:53:54 +0000

    Add camera zoomer script
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ Assets/Camera/Zoomer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class Zoomer : MonoBehaviour
+{
+    [SerializeField]
+    Camera Camera;
+
+    void Start()
+    {
+        this.Camera = this.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        var currentSize = this.Camera.orthographicSize;
+        var newSize = Mathf.Min(40, Mathf.Max(15, currentSize - (-1 * Input.mouseScrollDelta.normalized.y * 100 * Time.deltaTime)));
+        this.Camera.orthographicSize = newSize;
+    }
+}
