f5d74d6a03262ea65ebaf8a87fd9691f2266e3e7
m4rc3l05 <joaobat@live.com.pt>
2020-11-17 23:15:33 +0000

    Add base ai scripts for mobs
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ Assets/Mobs/Merikh/Scripts/Mover.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField]
+    Transform Target;
+
+    [SerializeField]
+    float Speed = 0;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Mobs/Shared/Scripts/Aimer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShootTargeter))]
+public class Aimer : MonoBehaviour
+{
+    [SerializeField]
+    ShootTargeter ShootTargeter;
+
+    void Start()
+    {
+        this.ShootTargeter = this.GetComponent<ShootTargeter>();
+    }
+
+    void Update()
+    {
+
+    }
+}
