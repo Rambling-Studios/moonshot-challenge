8d79ae9d8cc83de89eef58315c6d30034777b5cb
m4rc3l05 <joaobat@live.com.pt>
2020-11-17 23:15:33 +0000

    Add base ai scripts for mobs
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ Assets/Mobs/Shared/Scripts/ShootTargeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootTargeter : MonoBehaviour
+{
+    [SerializeField]
+    Transform Target;
+
+    public Transform getTarget() => this.Target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Mobs/Shared/Scripts/Shooter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ShootTargeter))]
+public class Shooter : MonoBehaviour
+{
+    [SerializeField]
+    ShootTargeter ShootTargeter;
+
+    [SerializeField]
+    GameObject Bullet;
+
+    void Start()
+    {
+        this.ShootTargeter = this.GetComponent<ShootTargeter>();
+    }
+
+    void Update()
+    {
+
+    }
+}
