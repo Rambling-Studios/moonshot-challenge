551a7acef1c3fdd90d2fdf1dfdf72cb1afd83cf8
m4rc3l05 <joaobat@live.com.pt>
2020-11-23 21:09:58 +0000

    Add custom skyblock
    Signed-off-by: m4rc3l05 <joaobat@live.com.pt>

---
+++ Assets/Scenes/MainScene/Skyblock/SkyBlockRotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyBlockRotator : MonoBehaviour
+{
+    public float Speed = 0.3f;
+
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", this.Speed * Time.time);
+    }
+}
